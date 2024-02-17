aac26a89b3e43f38c71b553363aba9ef58a07bba
kanishkaarunanandh <kanishka0000012@gmail.com>
2024-02-17 20:58:49 +0530

    Updated all files

---
+++ demo.js
@@ -0,0 +1,35 @@
+{/* <tr>
+<td>Name</td>
+<td><input type="text"></td>
+</tr>
+<tr>
+<td>Age</td>
+<td><input type="number"></td>
+</tr>
+<tr>
+<td>Gender</td>
+<td>
+    <input type="radio" name="Gender">male
+    <input type="radio" name="Gender">female
+</td>
+
+</tr>
+<tr>
+<td>Course</td>
+<td>
+    <select>
+        <option>HTML</option>
+        <option>CSS</option>
+        <option>JavaScript</option>
+        <option>Front-End</option>
+        <option>Back-End</option>
+    </select>
+</td>
+</tr>
+<tr>
+<td>E-mail</td>
+<td><input type="email"></td>
+</tr>
+<tr>
+<td><button>save</button></td>
+</tr> */}
